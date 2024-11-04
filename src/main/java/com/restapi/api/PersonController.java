553fc341e619eed45760fef0d5907f048fd270f9
veeraganesh vijayaraman <veeraganesh12@gmail.com>
2024-11-04 21:49:15 +0530

    sb restapi  project changes

--- src/main/java/com/restapi/api/PersonController.java
+++ src/main/java/com/restapi/api/PersonController.java
@@ -1,9 +1,24 @@
 package com.restapi.api;
 
+import com.restapi.model.Person;
 import com.restapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("v1/api/")
 public class PersonController {
 
+
+    @Autowired
     private PersonService personService;
+
+    @PostMapping
+    public String addPerson(Person person) {
+        personService.addPerson(person);
+        return "added";
+    }
 
 }
